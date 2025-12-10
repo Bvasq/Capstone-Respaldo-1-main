79c45473ea11f85815a0e45ca6921dd7da4f5cc3
Bastian Vasquez <128827019+Bvasq@users.noreply.github.com>
2025-12-10 01:16:01 -0300

    Update urls.py

--- ventas/urls.py
+++ ventas/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("rapida/", views.rapida, name="rapida_alt"),
     path("buscar/", views.buscar_productos, name="buscar"),
     path("confirmar/", views.confirmar_venta, name="confirmar"),
-    path("ticket/<int:venta_id>/download/", views.ticket_txt_download, name="ticket_txt_download")
+    path("ticket/<int:venta_id>/txt/", views.ticket_txt, name="ticket_txt"),
 ]
